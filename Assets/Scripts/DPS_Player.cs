b44bcd0c60281f2f929fcda9df26faa252d0f7d4
Gustavo <37023212+Orb23@users.noreply.github.com>
2020-02-29 16:38:56 -0800

    Add files via upload

---
+++ Assets/Scripts/DPS_Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DPS_Player : MonoBehaviour
+{
+    public string unitName;
+
+    public int damage;
+
+    public int maxHP;
+    public int currentHP;
+
+}

b4398284ada9a93fea718267aae429afb7d6feb1
Gustavo <37023212+Orb23@users.noreply.github.com>
2020-03-04 10:42:14 -0800

    Add files via upload

---
+++ Assets/Scenes/SceneController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void enterBattle()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        Debug.Log("Touching");
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("Game is now closed");
+        Application.Quit();
+    }
+
+}
